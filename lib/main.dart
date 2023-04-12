28ed11dd81fdf32f621a7897ae7cf070c046d2b4
VeronicaG19 <veronica.garcia.condor@gmail.com>
2023-04-12 16:28:03 -0600

    vgm

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Github Actions'),
+      home: MyHomePage(title: 'Github Actions in gitlab local'),
     );
   }
 }
